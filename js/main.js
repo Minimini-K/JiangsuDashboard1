3635812aa92897b511a78be2e1c34d2bd766941b
Hui Zhang <40585839+Minimini-K@users.noreply.github.com>
2021-07-14 22:23:01 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -112,8 +112,8 @@ function updateValues(cate) {
             //lable4.style = " font-size:12px;background: white;position: relative; top:-13px;left: 5px;color:#454545;font-weight:bold;"
             lable4.style = " font-size:14px;background: white;position: relative; top:-13px;left: -2px;color:#696969;font-weight:normal;"
             button.style = " font-size:12px;position: relative; top:-12px;left:-5px;font-weight:bold;height:20px"
-            lable2.style = "font-size:18px;margin-top:-10px;margin-left:40%;font-weight:bold;color:#696969;"
-            lable3.style = "font-size:13px;margin-top:-32px;margin-right:3px;color:#696969;text-align: right;"
+            lable2.style = "font-size:18px;margin-top:-12px;margin-left:40%;font-weight:bold;color:#696969;"
+            lable3.style = "font-size:13px;margin-top:-32px;margin-right:3px;color:#696969;text-align: right;font-weight:normal;"
             lable1.innerHTML = csvdata[i].name;
             lable4.innerHTML = csvdata[i].name;
             //button.innerText = csvdata[i].name;
